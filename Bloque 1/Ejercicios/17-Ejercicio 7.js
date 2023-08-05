4fde563762d3216e645bc0f74a76e59d99b67bbc
TenebrisMessor <arielhryo@hotmail.com>
2023-08-05 17:41:36 -0600

    recreating everything

--- Bloque 1/Ejercicios/17-Ejercicio 7.js
+++ Bloque 1/Ejercicios/17-Ejercicio 7.js
@@ -9,6 +9,15 @@ while(isNaN(numero)){
     numero = parseInt(prompt("Introduce un numero", 0));
 }
 
+document.write("<h3>Tabla del "+numero+"</h3>")
+for(i=1; i<=10; i++){
+    document.write(numero +"x" + i + "=" + (numero*i) + "<br>" )
+}
+
+
+
+
+//Todas las tablas
 for(var j=1; j<=10; j++){
     document.write("<h3>Tabla del "+j+"</h3>")
 for( var i=1; i<=10; i++){
