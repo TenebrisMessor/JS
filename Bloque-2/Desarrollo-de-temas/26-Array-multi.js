4309e51677463f427e2ec6546196a48a63672643
TenebrisMessor <arielhryo@hotmail.com>
2023-08-06 14:12:44 -0600

    Array multidimensiona

--- Bloque-2/Desarrollo-de-temas/26-Array-multi.js
+++ Bloque-2/Desarrollo-de-temas/26-Array-multi.js
@@ -1,1 +1,8 @@
 'use strict'
+
+var categorias = ['accion', 'terror', 'comedia'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran torino'];
+
+var cine = [categorias, peliculas];
+console.log(cine[0][1]); //El pirmer indice indica a que array se esta accediendo dentro del array multidimencional que en este caso esc categorias y el segundo muestra el indice dentro de ese otro array en este caso terror
+console.log(cine[1][0]); // Entra en el array peliculas en el indice La verdad duele.
