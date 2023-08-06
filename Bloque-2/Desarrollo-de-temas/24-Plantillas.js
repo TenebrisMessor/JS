8a0a9cdc9f7f628421dc7098ca133a836986092a
TenebrisMessor <arielhryo@hotmail.com>
2023-08-05 23:06:18 -0600

    funciones de texto

---
+++ Bloque-2/Desarrollo-de-temas/24-Plantillas.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//Plantillas de texto
+
+var nombre = prompt("METE TU NOMBRE");
+var apellidos = prompt("METE TUS APELLIDOS");
+
+var texto = "Mi nombre es: "+nombre+ " <br> Mis apellidos son: "+
