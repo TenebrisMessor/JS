590538d6d52f32a203eec3f3e8cb7ca9b7aafef3
TenebrisMessor <arielhryo@hotmail.com>
2023-08-13 03:54:45 -0600

    lo que llevo

--- Bloque-3/Ejercicios/28-dom.js
+++ Bloque-3/Ejercicios/28-dom.js
@@ -4,16 +4,25 @@
 
 
 var cambiaColor = (color) =>{
-    caja.style.background = color;
+    caja.style.background = color; 
 } 
+//conseguir elemenots con un ID concreto
+//Conseguir elementos por su etiqueta 
+var todoslosDivs = document.getElementsByTagName('div');
+console.log(todoslosDivs);
+var contenidoentexto = todoslosDivs[2].textContent;
 
-//var caja = document.getElementById("micaja");
-var caja = document.querySelector("#micaja");
-caja.innerHTML = "TEXTO EN LA CAJA DESDE JS!!"
-caja.style.background = "red";
-caja.style.padding = "20px";
-caja.style.color = "white";
-caja.className = "hola hola2";
+
+// Conseguir elementos por su clase
+
+
+//var caja = document.getElementById("micaja");  // usando getelementbyid podemos solo buscar usando el id
+var caja = document.querySelector("#micaja"); // usando el id estamos agregando lo que hay dentro del div con el id micaja a una variable
+caja.innerHTML = "TEXTO EN LA CAJA DESDE JS!!" //cambiando el texto desde js
+caja.style.background = "red"; // agregando background color desde js
+caja.style.padding = "20px"; //agregando padding desde js
+caja.style.color = "white";// agregando color al texto desde js
+caja.className = "hola hola2"; //agregando clases desde js
 
 
 
