f721005a2e041f09940c5524e9c7bc8a18240ced
TenebrisMessor <arielhryo@hotmail.com>
2023-08-24 07:16:36 -0600

    DOM

--- Bloque-3/Ejercicios/28-dom.js
+++ Bloque-3/Ejercicios/28-dom.js
@@ -8,9 +8,33 @@ var cambiaColor = (color) =>{
 } 
 //conseguir elemenots con un ID concreto
 //Conseguir elementos por su etiqueta 
-var todoslosDivs = document.getElementsByTagName('div');
-todoslosDivs.forEach((valor,indice) => {
-    
+// var todoslosDivs = document.getElementsByTagName('div'); //En esta línea, estás seleccionando todos los elementos <div> en el documento y almacenándolos en la variable todoslosDivs.
+
+
+/**************************************************************************************************UTILIZANDO FOR IN*******************************************/ 
+
+// var valor;
+// for(valor in todoslosDivs){ //Estás usando un bucle for...in para iterar a través de los elementos en el objeto todoslosDivs. Sin embargo, es importante destacar que este bucle no es la forma más adecuada de iterar a través de los elementos en un HTMLCollection (que es lo que devuelve getElementsByTagName). Aunque se puede hacer, hay mejores enfoques.
+//     console.log(todoslosDivs[valor]);
+//     if(typeof todoslosDivs[valor].textContent == 'string'){
+//         var parrafo = document.createElement("p"); // Aquí estás creando un nuevo elemento <p> (párrafo) utilizando document.createElement.
+//         var texto = document.createTextNode(todoslosDivs[valor].textContent); //Estás creando un nodo de texto utilizando el contenido de texto de cada elemento <div>. todoslosDivs[valor] accede al elemento <div> actual en cada iteración del bucle y .textContent recupera el contenido de texto dentro de ese elemento.
+//         parrafo.append(texto);//Estás agregando el nodo de texto (que contiene el contenido de texto del <div>) como hijo del elemento <p> que creaste antes.
+//         document.querySelector("#miseccion").prepend(parrafo);// Aquí, estás seleccionando un elemento con el ID "miseccion" (probablemente un contenedor donde deseas mostrar los párrafos) utilizando document.querySelector. Luego, estás agregando el elemento <p> que creaste como hijo de este elemento.
+//     }
+// }
+
+/**************************************************************************************************UTILIZANDO FOR EACH*******************************************/ 
+var todoslosDivs = Array.from(document.getElementsByTagName('div')); // Obtienes un HTMLCollection de elementos div
+var valor;
+todoslosDivs.forEach(valor => {
+    console.log(valor);
+    if (typeof valor.textContent === 'string') {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(valor.textContent);
+        parrafo.appendChild(texto);
+        document.querySelector("#miseccion").prepend(parrafo);
+    }
 });
 
 
